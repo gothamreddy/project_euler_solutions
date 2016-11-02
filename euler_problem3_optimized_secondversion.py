2d7f5cf6fa4055ea4b63555769f9e7f948c8ef76
gotham <developeracc@icloud.com>
2016-11-01 21:38:23 -0500

    modified euler_problem3_optimized_secondversion.py

--- euler_problem3_optimized_secondversion.py
+++ euler_problem3_optimized_secondversion.py
@@ -1,11 +1,12 @@
-#Euclid_Problem3.py
+#euler_problem3.py
 import math
-def Euler3(n=600851475143):
+def euler_problem3(n=600851475143):
     for i in range(2,100000):
         while n % i == 0:
             n //= i
-            print("Yay, %d is a factor, now we should test %d" % (i, n))
+            print("%d is a factor, now we should test %d" % (i, n))
             if n == 1 or n == i:
                 return i
 
-Euler3()
+euler_problem3()
+
